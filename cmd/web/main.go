f8680a8e745dcfe0205adc81dae223bb523bfc1a
Michael <michaelcclary@gmail.com>
2022-10-16 13:26:58 -0500

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/michaelcclary/bookings/pkg/config"
+	"github.com/michaelcclary/bookings/pkg/handlers"
+	"github.com/michaelcclary/bookings/pkg/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function
+func main() {
+
+	//change to true in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+
+	if err != nil {
+		fmt.Printf("Error %s", err)
+		log.Fatal("cannot create template cache")
+
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	render.NewTemplates(&app)
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/michaelcclary/bookings/pkg/config"
+	"github.com/michaelcclary/bookings/pkg/models"
+	"github.com/michaelcclary/bookings/pkg/render"
+)
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	fmt.Print(remoteIP)
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	//perform logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	//send data to the template
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
